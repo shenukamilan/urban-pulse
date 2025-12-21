f92ee851fb28f3f3d9a925a5328af137f1d4f9c6
Shenuka <shenukamilan@gmail.com>
2025-12-21 11:39:59 +0530

    Add PropertyInfoBar component to PropertyPage

---
+++ src/components/PropertyInfoBar/PropertyInfoBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Heart } from 'lucide-react';
+import './PropertyInfoBar.css';
+
+const PropertyInfoBar = ({ property, isFavourite }) => {
+
+    return (
+        <section className="info-bar-container">
+            <div className="info-left-content">
+                {/* Price */}
+                <h2 className="info-price">£{property.price.toLocaleString()}</h2>
+
+                {/* Metadata Line */}
+                <p className="info-meta">
+                    {property.type} • {property.bedrooms} Bedrooms • {property.tenure}
+                </p>
+
+                {/* Address Line */}
+                <p className="info-address">{property.location}</p>
+            </div>
+
+            <div className="info-right-content">
+                <button
+                    className={`info-fav-btn ${isFavourite ? 'active' : ''}`}
+                    onClick={() => console.log("clicked")}
+                >
+                    <Heart
+                        size={18}
+                        fill={isFavourite ? "white" : "none"}
+                        className="info-heart-icon"
+                    />
+                    <span>{isFavourite ? "Remove from Favourites" : "Add to Favourites"}</span>
+                </button>
+            </div>
+        </section>
+    );
+};
+
+export default PropertyInfoBar;
--- src/pages/PropertyPage/PropertyPage.jsx
+++ src/pages/PropertyPage/PropertyPage.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import propertiesData from "../../data/properties.json"
 import PropertyHeader from '../../components/PropertyHeader/PropertyHeader';
+import PropertyInfoBar from '../../components/PropertyInfoBar/PropertyInfoBar';
+import './PropertyPage.css';
 
 
 function PropertyPage() {
 
-  let property = propertiesData.properties[0];
+  let currentProperty = propertiesData.properties[0];
   return (
     <>
+
       <PropertyHeader
-        location={property.location}
+        location={currentProperty.location}
         onBack={() => console.log("Go back")}
       />
+      <div className="property-page-layout">
+        <PropertyInfoBar
+          property={currentProperty}
+          isFavourite={true}
+        />
+      </div>
+
     </>
   )
 }
